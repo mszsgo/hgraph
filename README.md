d0252c908ecc39793a5c76acc13ddd048a6e52c2
zengs <zengs@unionlive.com>
2019-12-08 22:53:21 +0800

    feign

---
+++ README.md
@@ -0,0 +1,11 @@
+# hgraph  
+
+ Graphql 
+ 
+ 
+ 
+ 
+ # Change Log
+ 
+ 
+ ## v0.0.1 
---
+++ parse_test.go
@@ -0,0 +1,11 @@
+package hgraph
+
+import (
+	"testing"
+)
+
+func TestParseGraphqlMicroService(t *testing.T) {
+	ParseGraphqlMicroService("query {member(){list(){}   ,  total(){}}  , \r\n points(){list(){},total(){}}\r\n,svf(){list(){\r\n},total(){}}}")
+	ParseGraphqlMicroService("query findMemeber {member(){list(){}   ,  total(){}}  }")
+	ParseGraphqlMicroService("mutation {member(){list(){}   ,  total(){}}  }")
+}
