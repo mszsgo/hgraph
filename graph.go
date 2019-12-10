3b7e1f8608661986b9c1007f620255ae0f0787c0
zengs <zengs@unionlive.com>
2019-12-10 08:02:42 +0800

    feign

--- graph.go
+++ graph.go
@@ -1,55 +1,27 @@
+// Graphql 类型缩写, 减少Schema代码量
 package hgraph
 
 import (
-	"encoding/json"
-	"log"
+	"github.com/graphql-go/graphql"
 )
 
-// Graphql 请求JSON Model
-type GraphRequestModel struct {
-	RequestId     string      `json:"requestId"`
-	Token         string      `json:"token"`
-	OperationName string      `json:"operationName"`
-	Query         string      `json:"query"`
-	Variables     interface{} `json:"variables"`
+var ArgString = func(description string) *graphql.ArgumentConfig {
+	return &graphql.ArgumentConfig{Type: graphql.String, DefaultValue: nil, Description: description}
+}
+var ArgNonNullString = func(description string) *graphql.ArgumentConfig {
+	return &graphql.ArgumentConfig{Type: graphql.NewNonNull(graphql.String), DefaultValue: nil, Description: description}
+}
+var ArgInt = func(description string) *graphql.ArgumentConfig {
+	return &graphql.ArgumentConfig{Type: graphql.Int, DefaultValue: nil, Description: description}
+}
+var ArgNonNullInt = func(description string) *graphql.ArgumentConfig {
+	return &graphql.ArgumentConfig{Type: graphql.NewNonNull(graphql.Int), DefaultValue: nil, Description: description}
 }
 
-var ParseGraphqlReuqest = func(reqJson []byte) *GraphRequestModel {
-	var model *GraphRequestModel
-	err := json.Unmarshal(reqJson, &model)
-	if err != nil {
-		log.Print(err)
-	}
-	return model
-}
-
-// Graphql 响应JSON Model
-type GraphResponseModel struct {
-	RequestId string                   `json:"requestId"`
-	HostTime  string                   `json:"hostTime"`
-	Data      map[string]interface{}   `json:"data"`
-	Errors    []map[string]interface{} `json:"errors,omitempty"`
-}
-
-// 网关调用服务
-func Gateway(body []byte) []byte {
-	reqModel := ParseGraphqlReuqest(body)
-	bytes, err := json.Marshal(Graphql(reqModel))
-	if err != nil {
-		log.Print(err)
-	}
-	return bytes
-}
-
-// 调用业务服务
-// 解析请求字符串第一级字段作为服务名调用Graphql服务
-func Graphql(model *GraphRequestModel) *GraphResponseModel {
-	services := ParseGraphqlQuery(model.Query)
-	return BulkRequest(&GraphRequestModel{
-		RequestId:     model.RequestId,
-		Token:         model.Token,
-		OperationName: model.OperationName,
-		Query:         "",
-		Variables:     model.Variables,
-	}, services)
+var NewObject = func(name string, fields graphql.Fields, description string) *graphql.Object {
+	return graphql.NewObject(graphql.ObjectConfig{
+		Name:        name,
+		Fields:      fields,
+		Description: description,
+	})
 }
